20dd4a95eb0ac974105bf66c310df53149eee791
Syed Ahmed <ahmedasad2010@gmail.com>
2019-07-31 23:32:44 +0500

    task done:typing status is a feature now

--- app/src/main/java/com/example/firebasechatapp/Controller/ChatRoomActivity.kt
+++ app/src/main/java/com/example/firebasechatapp/Controller/ChatRoomActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.firebasechatapp.Adapter_Groupie.ChatItemFrom
 import com.example.firebasechatapp.Adapter_Groupie.ChatItemTo
@@ -16,10 +17,7 @@ import com.example.firebasechatapp.Model.ChatMessage
 import com.example.firebasechatapp.Utility.USER_KEY
 import com.example.firebasechatapp.Utility.UserData
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_chat_room.*
@@ -43,18 +41,93 @@ class ChatRoomActivity : AppCompatActivity() {
         messagesListView.layoutManager = LinearLayoutManager(this)
         messagesListView.scrollToPosition(adapter.itemCount - 1)
         hideKeyBoard()
-        println("tiem ::: "+returnDateString( Calendar.getInstance().time.toString()))
+        println("tiem ::: " + returnDateString(Calendar.getInstance().time.toString()))
+
+        FirebaseDatabase.getInstance().getReference("/current-chat/${FirebaseAuth.getInstance().uid}/focus").push()
+        checkIfFocus()
+        checkIfUserIsTyping()
+
+
     }
+
+    override fun onDestroy() {
+        val ref = FirebaseDatabase.getInstance().getReference("/current-chat/${FirebaseAuth.getInstance().uid}")
+        ref.removeValue()
+        super.onDestroy()
+    }
+
+    override fun onPause() {
+        val ref = FirebaseDatabase.getInstance().getReference("/current-chat/${FirebaseAuth.getInstance().uid}")
+        ref.removeValue()
+        super.onPause()
+    }
+
+
+    private fun checkIfUserIsTyping() {
+        val status = findViewById<TextView>(R.id.txtstatus)
+        val user = intent.getParcelableExtra<User>(USER_KEY)
+        val toId = user.uid
+        val ref = FirebaseDatabase.getInstance().getReference("/current-chat/$toId/focus")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+//                val status = p0.getValue()
+
+
+                if(p0.getValue()!=null)
+                {
+                    var state = p0.getValue()
+                    if(p0.getValue().toString() == "true"){
+                        println("status:: true "+p0.getValue() )
+                        status.text = "typing..."
+                        hideKeyBoard()
+                    }
+                    else if(p0.getValue().toString() != "true"){
+                        println("status:: false "+p0.getValue() )
+                        status.text = ""
+                        hideKeyBoard()
+                    }
+                }
+
+
+
+            }
+            //
+            override fun onCancelled(p0: DatabaseError) {}
+        })
+
+    }
+
+    private fun focusStatus(focus: Boolean) {
+
+        val fromId = FirebaseAuth.getInstance().uid
+
+        val ref = FirebaseDatabase.getInstance().getReference("/current-chat/$fromId/focus")
+
+        if (!focus) {
+            ref.setValue("false")
+                .addOnSuccessListener {
+                    println("focused")
+                }
+        } else {
+            ref.setValue("true")
+                .addOnSuccessListener {
+                    println("not focused")
+                }
+        }
+
+    }
+
 
     fun sendMsgBtnClicked(view: View) {
         performSendMessage()
     }
 
-    lateinit var currentDate:String
+
+    lateinit var currentDate: String
     var date = ""
-    lateinit var currentday :String
+    lateinit var currentday: String
     var day = ""
-     private fun listenForMessages() {
+    private fun listenForMessages() {
 
         val fromId = FirebaseAuth.getInstance().uid
         val toId = toUser?.uid.toString()
@@ -70,13 +143,11 @@ class ChatRoomActivity : AppCompatActivity() {
 
                 currentDate = chatMessage!!.timeStampt
                 currentday = currentDate.split(",")[0]
-                if(date.isEmpty()){
+                if (date.isEmpty()) {
                     date = currentDate
                     day = currentday
                     adapter.add(DateAdapter(day))
-                }
-
-                else if(day != currentday){
+                } else if (day != currentday) {
                     date = currentDate
                     day = currentday
                     adapter.add(DateAdapter(day))
@@ -85,19 +156,16 @@ class ChatRoomActivity : AppCompatActivity() {
 
                 if (chatMessage?.text != null) {
 
-                        if (chatMessage.fromId == fromId && chatMessage.text.isNotEmpty()) {
+                    if (chatMessage.fromId == fromId && chatMessage.text.isNotEmpty()) {
 
-                            val currentUser = UserData.currentUser ?: return
-                            adapter.add(ChatItemTo(chatMessage.text, currentUser,currentDate.split(",")[1]))
-                        } else {
-                            //                        println("chat message: ${chatMessage.text}")
-                            adapter.add(ChatItemFrom(chatMessage.text, toUser!!,currentDate.split(",")[1]))
-                        }
-                        messagesListView.scrollToPosition(adapter.itemCount - 1)
+                        val currentUser = UserData.currentUser ?: return
+                        adapter.add(ChatItemTo(chatMessage.text, currentUser, currentDate.split(",")[1]))
+                    } else {
+                        //                        println("chat message: ${chatMessage.text}")
+                        adapter.add(ChatItemFrom(chatMessage.text, toUser!!, currentDate.split(",")[1]))
                     }
-
-
-                else{
+                    messagesListView.scrollToPosition(adapter.itemCount - 1)
+                } else {
                     println("chat is null")
                 }
             }
@@ -156,7 +224,7 @@ class ChatRoomActivity : AppCompatActivity() {
 
             val latestMessageRef = FirebaseDatabase.getInstance().getReference("/latest-messages/$fromId/$toId")
             latestMessageRef.setValue(chatMessage)
-            val latestMessageToRef= FirebaseDatabase.getInstance().getReference("/latest-messages/$toId/$fromId")
+            val latestMessageToRef = FirebaseDatabase.getInstance().getReference("/latest-messages/$toId/$fromId")
             latestMessageToRef.setValue(chatMessage)
         }
 
@@ -173,16 +241,30 @@ class ChatRoomActivity : AppCompatActivity() {
         }
     }
 
-    fun returnDateString(isoString:String):String{
+    fun returnDateString(isoString: String): String {
         val isoFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
         isoFormatter.timeZone = TimeZone.getTimeZone("UTC")
         var convertDate = Date()
         try {
             convertDate = isoFormatter.parse(isoString)
-        }catch (e: Exception){
-            Log.d("parse","can not parse Date")
+        } catch (e: Exception) {
+            Log.d("parse", "can not parse Date")
         }
-        val outDateString = SimpleDateFormat("E,h:mm a",Locale.getDefault())
+        val outDateString = SimpleDateFormat("E,h:mm a", Locale.getDefault())
         return outDateString.format(convertDate)
+    }
+
+    private fun checkIfFocus() {
+        sendtxtMessage.setOnFocusChangeListener(object : View.OnFocusChangeListener {
+            override fun onFocusChange(view: View, hasFocus: Boolean) {
+                if (!hasFocus) {
+                    focusStatus(false)
+                } else {
+                    focusStatus(true)
+                }
+
+            }
+        })
+
     }
 }
